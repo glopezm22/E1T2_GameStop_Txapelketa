b0c64f73e77161547711da893ca5fa1c9fd99f59
Gaizkagb <ggorrochateguib24@fpsanturtzilh.eus>
2024-10-29 08:57:42 +0100

    Insercion de Cookies

---
+++ cookies.js
@@ -0,0 +1,68 @@
+//Creador de Cookies
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+setCookie("user", "Juan", 7);
+
+//Como Leer una Cookie
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+const user = getCookie("user");
+console.log(user); 
+
+//Borrador de Cookies
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+eraseCookie("user");
+
+//Avisador de Cookies
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function acceptCookies() {
+    setCookie("cookiesAccepted", "true", 30); 
+    document.getElementById("cookieConsent").style.display = "none";
+}
+
+// Mostrar el aviso si el usuario no ha aceptado previamente las cookies
+window.onload = function() {
+    if (!getCookie("cookiesAccepted")) {
+        document.getElementById("cookieConsent").style.display = "block";
+    }
+};
